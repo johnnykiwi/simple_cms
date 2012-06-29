71c12b86ff61e1fda633cdbff19851d044295670
Your Name <johnnykiwi1@gmail.com>
2012-06-28 23:54:38 -0400

    inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'a1608883b59ed01558cdebea7bf96d6da4e1bf4b8955d1734a79514fb9f5208f9096b0c8a4e25759d4bfea14311f4604dfe11e26bc94c85201af1a134c44dcf0'
